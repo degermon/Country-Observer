9a33da85d4e166d9b6773845b214f61f47116cc1
degermon <degermon@gmail.com>
2020-06-25 18:17:32 +0300

    Initial Commit

---
+++ Country Observer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Country Observer
+//
+//  Created by Daniel Šuškevič on 2020-06-25.
+//  Copyright © 2020 Daniel Šuškevič. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
